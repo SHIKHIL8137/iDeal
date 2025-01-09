2eaf4d49a62d839efea3b43d6b74f275cdf22834
SHIKHIL8137 <shikhilks870@gmail.com>
2025-01-09 09:37:37 +0530

    add mongoose file in profile controller

--- controller/user/userProfileController.js
+++ controller/user/userProfileController.js
@@ -3,7 +3,7 @@ const {User} = require('../../model/user/userModel')
 const {Address} = require('../../model/user/addressModel')
 require('dotenv').config()
 const bcrypt = require('bcrypt');
-
+const mongoose = require('mongoose');
 
 // rendering the route for profile
 
